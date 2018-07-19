23c6548d52eae0fe823f697202ccb69e5e21554d
unknown <nishushetty151997@gmail.com>
2018-07-19 16:02:15 +0530

    initial commit

--- js/regform.js
+++ js/regform.js
@@ -1,12 +1,13 @@
 $(function(){
-  $("#regform").validate({
+    $(".submit").click(function(){ 
+       var isValid = $("#regform").validate({
       rules:{
           usn:{
               required:true,
           },
           name:{
               required:true,
-              minlength:10
+              minlength:2
           },
           email:{
               required:true,
@@ -16,10 +17,10 @@ $(function(){
              required:true
          },
           percentage:{
-              required:true;
-              
+              required:true,
+              min:55,
+              max:100
           }
-          
       },
           messages:{
               usn:{
@@ -33,14 +34,38 @@ $(function(){
                   required:'email canot be empty'
               },
               mobile:{
-                  required:'mobile number canot be empty'
+                  required:'mobile cannot be empty'
               },
               percentage:{
-                  required:
-              }
-              
+                  required:'percentage canotbe empty',
+                min:'below 55 not be eligible',
+                  max:'eligible'
+              } 
           }
-  }); 
+  }).form(); 
+        if(isValid){
+            var usn = $("#usn").val();
+            var name = $("#name").val();
+             var email = $("#email").val();
+             var mobile = $("#mobile").val();
+             var course = $("#course").val();
+             var percentage = $("#percentage").val();
+            $(".reset").click();
+            
+            student = {
+                "usn":usn,
+                "name":name,
+                "email":email,
+                "mobile":mobile,
+                "course":course,
+                "percentage":percentage
+            }
+            console.log(student);
+            return false;
+        }
     
+        
+    });
+  
     
 });
